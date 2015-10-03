47dffcea0a85f0d4c8ddeca4906a33c1bd1cd10a
Guillaume Lemaitre <glemaitre@visor.udg.edu>
2015-10-03 02:27:58 +0200

    Add the paper of glemaitre and mrastgoo

---
+++ glemaitre/auto/method.el
@@ -0,0 +1,17 @@
+(TeX-add-style-hook "method"
+ (lambda ()
+    (LaTeX-add-labels
+     "fig:ML-scheme"
+     "sec:method"
+     "subsec:prepro"
+     "subfig:vol"
+     "subfig:raw"
+     "subfig:nlm"
+     "fig:denoise"
+     "subsec:feaext"
+     "Eq:LBP"
+     "subfig:lbp"
+     "subfig:lbptop"
+     "fig:lbp"
+     "subsec:fearep")))
+
---
+++ mrastgoo/content/method/auto/material-method.el
@@ -0,0 +1,10 @@
+(TeX-add-style-hook "material-method"
+ (lambda ()
+    (LaTeX-add-labels
+     "sec:mm"
+     "fig:schema"
+     "sec:dataset"
+     "sec:feat"
+     "Eq:AngO"
+     "Eq:Gabor")))
+
