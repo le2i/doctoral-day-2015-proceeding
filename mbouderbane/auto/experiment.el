3d4db8af8b3b13c293cb798e4a3b66371d96dec3
Guillaume Lemaitre <glemaitre@visor.udg.edu>
2015-10-06 20:35:57 +0200

    Finalise the proceeding

---
+++ mbouderbane/auto/experiment.el
@@ -0,0 +1,8 @@
+(TeX-add-style-hook "experiment"
+ (lambda ()
+    (LaTeX-add-labels
+     "sec:exp"
+     "tab:SERI-data"
+     "tab:Duke-data"
+     "tab:ComparisonRefandOurs")))
+
---
+++ mbouderbane/content/method/auto/BoFFramework.el
@@ -0,0 +1,8 @@
+(TeX-add-style-hook "BoFFramework"
+ (lambda ()
+    (LaTeX-add-labels
+     "fig:BoF-framework")
+    (TeX-add-symbols
+     "blockdist"
+     "edgedist")))
+
